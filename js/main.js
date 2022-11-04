94347f6f518d0c686451a5b9bc29ad3a77f9cd11
zakirovravshan <zakirovrovshan@gmail.com>
2022-11-05 02:34:28 +0500

    add document page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 const elButton = document.querySelector('.burger__btn')
 const elNavbar = document.querySelector('.navbar')
+const moreBtn = document.querySelector('.information__btn')
+const infoItems = document.querySelectorAll('.information__item')
+const infoList = document.querySelector('.information__list')
+const elArrow = document.querySelector('.arrow__span')
+
 
 elButton.addEventListener('click' , function(){
     elNavbar.classList.toggle('navbar--open')
+})
+moreBtn.addEventListener('click' , function(){
+    infoItems.forEach((items) =>{
+        items.classList.toggle('information__item--active')
+        
+        if(items.classList.contains('information__item--active')){
+            moreBtn.textContent = "Yopish"
+        }else{
+            moreBtn.textContent = "Barchasini ko'rish"
+        }
+    })
+    
+    
+    
+})
+
+window.addEventListener('scroll' , function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 400){
+        elArrow.classList.toggle('arrow__span--open')
+    }
 })
